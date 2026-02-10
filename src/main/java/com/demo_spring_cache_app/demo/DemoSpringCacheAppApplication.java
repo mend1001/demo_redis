c649b651d5ff3f26aaa7fd8a274191344db6ff69
mend1001 <mend1001mend1001@gmail.com>
2026-02-10 12:04:32 -0500

    first commit

---
+++ src/main/java/com/demo_spring_cache_app/demo/DemoSpringCacheAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo_spring_cache_app.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringCacheAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringCacheAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo_spring_cache_app/demo/DemoSpringCacheAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo_spring_cache_app.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringCacheAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
